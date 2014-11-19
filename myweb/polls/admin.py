9b478e552ee45be9e1ef4d487e37ba2ba9e31aa2
root <root@localhost.docker>
2014-11-19 11:24:13 +0800

    =Polls

--- myweb/polls/admin.py
+++ myweb/polls/admin.py
@@ -16,6 +16,8 @@ class PollAdmin(admin.ModelAdmin):
 		('Date information',{'fields':['pub_date'],'classes':['collapse']}),
 	]
 	list_display = ('question','pub_date','was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question']
 
 	inlines = [ChoiceInline]
 admin.site.register(Poll,PollAdmin)
